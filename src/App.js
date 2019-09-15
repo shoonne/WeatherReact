cf7604e2605f7b59480a5013933387b54c923598
Shaon Ahmed <shaon_ahmed@msn.com>
2019-09-15 15:31:22 +0200

    Changed text again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and  to reload.
+          Edit <code>src/App.js</code> an to reload.
         </p>
         <a
           className="App-link"
